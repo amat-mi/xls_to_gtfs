fed163d86b93e2804b0630f1acb2c98c5ce7cb91
francot <frenk.calza@gmail.com>
2016-03-03 14:38:50 +0100

    edit config_db

--- config_db.py
+++ config_db.py
@@ -1,7 +1,7 @@
 # CONNECTION PARAMETERS
 # TODO: PASS THEM FROM A CONFIG FILE OR FROM THE COMMAND LINE
-DB_NAME = "monitoraggio_tpl"
-DB_HOST = "192.1.1.115"
-DB_USER = "monitoraggio_tpl"
-DB_PASS = "monitoraggio_tpl"
+DB_NAME = ""
+DB_HOST = ""
+DB_USER = ""
+DB_PASS = ""
 DB_SCHEMA = "linee_s"
